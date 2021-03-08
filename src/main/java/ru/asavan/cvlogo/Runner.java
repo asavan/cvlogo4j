5129e19609690283521d3b2fffa029bc48e154ed
a.savanovich <asavanster@gmail.com>
2021-03-08 17:55:50 +0300

    beautify Runner

--- src/main/java/ru/asavan/cvlogo/Runner.java
+++ src/main/java/ru/asavan/cvlogo/Runner.java
@@ -9,7 +9,6 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDate;
-import java.time.Month;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -17,42 +16,63 @@ import java.util.function.Predicate;
  * Created by asavan on 21.05.2020.
  */
 public class Runner {
-    public static final String USERNAME = "asavan";
-    public static final String GITHUB_BASE_URL = "https://github.com/";
+    private static final String USERNAME = "asavan";
+    private static final String REPO_TO_DRAW = "cvlogo";
+    private static final String GITHUB_BASE_URL = "https://github.com/";
     private static final boolean DEBUG_PRINTING = true;
-    private static final LocalDate GITHUB_ERROR_SINCE = LocalDate.of(2020, Month.JULY, 4);
+    private static final LocalDate GITHUB_ERROR_SINCE = Parser.getLocalDate("2020-07-04"); // or null to switch off
 
     public static void main(String[] args) throws IOException {
-        draw(false, Pictures.strToSprite(Pictures.ASAVAN3));
+        asavanPic(false);
     }
 
-    private static void draw(boolean isNew, Integer[][] image) throws IOException {
+    public static void asavanPic(boolean isNew) throws IOException {
+        draw(isNew, Pictures.strToSprite(Pictures.ASAVAN3), 9, Color.ONE, 0);
+    }
+
+    public static void dot() throws IOException {
+        int offset = 0;
+        Integer[][] image = new Integer[1][1];
+        image[0][0] = 4;
+        draw(true, image, offset, null, 60);
+    }
+
+    public static void fill() throws IOException {
+        draw(false, null, 0, Color.ONE, 0);
+    }
+
+    private static void draw(boolean isNew, Integer[][] image, int offset, Color fillColor, int minFourColor) throws IOException {
         Calendar cal = getCalendar();
-        cal.setFillColor(Color.ONE);
-        // cal.setMinColor(Color.FOUR, 26);
+        if (fillColor != null) {
+            cal.setFillColor(fillColor);
+        }
+
+        if (minFourColor > 0) {
+            cal.setMinColor(Color.FOUR, minFourColor);
+        }
         if (DEBUG_PRINTING) {
             System.out.println(cal.minCountPrintable());
         }
 
-        int offset = 9;
         if (DEBUG_PRINTING && image != null) {
             System.out.println(Pictures.printImage(image));
         }
-        String repo = "cvlogo";
         OsName osName = chooseOs();
-        String output = Commiter.fake_it(image, cal, USERNAME, repo, offset, osName, isNew, GITHUB_ERROR_SINCE);
+        String output = Commiter.fake_it(image, cal, USERNAME, REPO_TO_DRAW, offset, osName, isNew, GITHUB_ERROR_SINCE);
         if (!output.isEmpty()) {
-            writeOnDisk(repo, output, osName);
+            writeOnDisk(REPO_TO_DRAW, output, osName, isNew);
         } else {
             System.out.println("No changes");
         }
     }
 
-    private static void writeOnDisk(String repo, String output, OsName osName) throws IOException {
+    private static void writeOnDisk(String repo, String output, OsName osName, boolean isNew) throws IOException {
         String scriptName = getFileName(repo, osName);
         save(output, scriptName);
         System.out.println(scriptName + " saved.");
-        System.out.println(String.format("Create a new(!) repo named %s at %s and run the script", repo, GITHUB_BASE_URL));
+        if (isNew) {
+            System.out.println(String.format("Create a new(!) repo named %s at %s and run the script", repo, GITHUB_BASE_URL));
+        }
     }
 
     private static Calendar getCalendar() {
@@ -76,20 +96,6 @@ public class Runner {
         return calendarExtractor;
     }
 
-    private static void dot() throws IOException {
-        Calendar cal = getCalendar();
-        cal.setMinColor(Color.FOUR, 60);
-        if (DEBUG_PRINTING) {
-            System.out.println(cal.minCountPrintable());
-        }
-        int offset = 0;
-        Integer[][] image = new Integer[1][1];
-        image[0][0] = 4;
-        String repo = "dotcvlogo6";
-        OsName osName = chooseOs();
-        String output = Commiter.fake_it(image, cal, USERNAME, repo, offset, osName, true, GITHUB_ERROR_SINCE);
-        writeOnDisk(repo, output, osName);
-    }
 
     private static void retrieveContributionsCalendar(Predicate<String> pred) {
         String base_url = GITHUB_BASE_URL + "users/" + USERNAME;
